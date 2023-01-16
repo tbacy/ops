e4ed845ad4076e6d2057408c3704fc8429891d95
tbacy <69180844+tbacy@users.noreply.github.com>
2023-01-16 02:53:23 -0600

    ps learn

---
+++ terraform/ps_learn/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Resources
+
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+
+    name = "my-vpc"
+    cidr = var.vpc_cidr_range
+
+    azs = slice(data.aws_availability_zones.azs.names, 0, 2)
+    public_subnets = var.public_subnets
+
+    # Database subnets
+    database_subnets = var.database_subnets
+    database_subnet_group_tags = {
+        subnet_type = "database"
+    }
+
+    tags = {
+        Environment = "dev"
+        Team        = "infra"
+    }
+  
+}
+
+
+# Outputs
+
+output "vpc_id" {
+    value = module.vpc.vpc_id
+
+}
+
+output "db_subnet_group" {
+    value = module.vpc.database_subnet_group
+  
+}
+
+output "public_subnets" {
+    value = module.vpc.public_subnets
+  
+}
--- terraform/server/main.tf
+++ terraform/server/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_vpc" "web_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
@@ -20,8 +20,8 @@ resource "aws_vpc" "web_vpc" {
   }
 }
 
-resource "aws_subnet" "web_subnet" {
-  vpc_id            = aws_vpc.web_vpc.id
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "172.16.10.0/24"
   availability_zone = "us-west-2"
 
@@ -33,15 +33,14 @@ resource "aws_subnet" "web_subnet" {
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.web_vpc.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.web_vpc.ipv6_cidr_block]
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
   }
 
   egress {
@@ -58,7 +57,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_network_interface" "nic" {
-  subnet_id   = aws_subnet.web_subnet.id
+  subnet_id   = aws_subnet.subnet.id
   private_ips = ["172.16.10.100"]
 
   tags = {
@@ -67,13 +66,13 @@ resource "aws_network_interface" "nic" {
 }
 
 # Create an EC2 instance
-resource "aws_instance" "devInstance" {
+resource "aws_instance" "Instance01" {
   ami = "ami-072bfa7f83b597e2b"
   instance_type = var.instance_type
   associate_public_ip_address = true
   
   network_interface {
-    network_interface_id = aws_network_interface.web_nic.id
+    network_interface_id = aws_network_interface.nic.id
     device_index         = 0
   }
 
